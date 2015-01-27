4844d0e45a979117840276d41f6fa8f8e85482c3
seth <sethhearld@gmail.com>
2015-01-27 17:32:03 -0500

    can manipulate points with mouse

--- drawBezier.js
+++ drawBezier.js
@@ -6,6 +6,7 @@ var context = canvas.getContext('2d');
 var Path = function(){
     //an object to store a series of Point objects.
     this.points = [];
+    this.select = null;
 }
 Path.prototype.draw = function() {
     //draw the bezier curve formed by the points in the Path
@@ -50,22 +51,62 @@ Path.prototype.draw = function() {
 
 var Point = function(x,y) {
     this.vec = new Vector(x,y);
+    this.radius = 5;
 }
-Point.prototype.draw = function(color, radius){
+Point.prototype.draw = function(color){
     context.beginPath();
-    context.arc(this.vec.coords()[0],this.vec.coords()[1], radius, 0, 2 * Math.PI, false);
+    context.arc(this.vec.coords()[0],this.vec.coords()[1], this.radius, 0, 2 * Math.PI, false);
     context.fillStyle = color;
     context.fill();
 }
+Point.prototype.contains = function(x,y) {
+    var fudge = 2;
+    var xdif = Math.abs(x - this.vec.coords()[0]);
+    var ydif = Math.abs(y - this.vec.coords()[1]);
+    if (xdif <= this.radius+fudge && ydif <= this.radius+fudge){
+        return true;
+    }
+}
 
+var num_clicks = 0;
+var overlaps = false;
 function makePoint(evt) {
     var pos = mousePos(evt);
-    if ((pos[0] > 0 && pos[0] < canvas.width) && (pos[1] > 0 && pos[1] < canvas.height)) {
-        var a = new Point(pos[0],pos[1]);
-        testPath.points.push(a);
-    } else {
-        return;
+    var x = pos[0];
+    var y = pos[1];
+    if ((x > 0 && x < canvas.width) && (y > 0 && y < canvas.height)) {
+        if ((x > 0 && x < canvas.width) && (y > 0 && y < canvas.height)) {
+            for (var i = 0; i < testPath.points.length; i++){
+                if (testPath.points[i].contains(x,y)){
+                    overlaps = true;
+                    testPath.select = testPath.points[i];
+                }
+            }
+        }
+        if (overlaps === false) {
+            num_clicks += 1;
+            var a = new Point(x,y);
+            if (num_clicks <= 2){
+                testPath.points.push(a);
+            } else {
+                a.radius = 2.5;
+                testPath.points.splice(num_clicks-2,0,a);
+            }
+        } else {
+            return;
+        }
     }
+}
+
+function testDrag(evt) {
+    if (overlaps) {
+        var pos = mousePos(evt);
+        testPath.select.vec = new Vector(pos[0], pos[1]);
+    }
+}
+
+function clearOverlaps(evt){
+    overlaps = false;
 }
 
 function mousePos(evt) {
@@ -78,18 +119,19 @@ function mousePos(evt) {
 
 var clearCanvas = function() {
     testPath.points = [];
+    num_clicks = 0;
     context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 function init() {
-    var p = new Point(10,390);
-    var r = new Point(10,10);
-    var s = new Point(590,10);
-    var q = new Point(590,390);
-    testPath.points.push(p);
-    testPath.points.push(r);
-    testPath.points.push(s);
-    testPath.points.push(q);
+    // var p = new Point(10,390);
+    // var r = new Point(10,10);
+    // var s = new Point(590,10);
+    // var q = new Point(590,390);
+    // testPath.points.push(p);
+    // testPath.points.push(r);
+    // testPath.points.push(s);
+    // testPath.points.push(q);
 }
 
 
@@ -97,10 +139,10 @@ function draw() {
     context.clearRect(0,0,canvas.width,canvas.height);
     // context.save();
     for (var i = 0; i < testPath.points.length; i++) {
-        if (i == 0 || i == testPath.points.length -1) {
-            testPath.points[i].draw('blue', 5);
+        if (i === 0 || i === testPath.points.length -1) {
+            testPath.points[i].draw('blue');
         } else {
-            testPath.points[i].draw('red', 2.5);
+            testPath.points[i].draw('red');
         }
     }
     if (testPath.points.length > 1){
@@ -113,14 +155,15 @@ function draw() {
 var testPath = new Path();
 
 function main() {
-    document.addEventListener('click', makePoint, false);
+    document.addEventListener('mousedown', makePoint, false);
+    document.addEventListener('mouseup', clearOverlaps, false);
     document.getElementById('clearButton').addEventListener('click', clearCanvas, false);
+    canvas.addEventListener('mousemove', testDrag);
 
     init();
 
-    var loop = function () {
+    var loop = function() {
         draw();
-
         window.requestAnimationFrame(loop, canvas);
     };
     window.requestAnimationFrame(loop, canvas);
